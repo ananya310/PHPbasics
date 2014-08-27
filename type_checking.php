013eb9da9d9421b2d44434f302c24af7f24f9943
Arindam Roy <arindamroy.roy619@live.com>
2014-08-28 03:51:04 +0530

    Create type_checking.php

---
+++ type_checking.php
@@ -0,0 +1,23 @@
+<?php
+
+/* 
+ * PHP type checking helper functions.
+*/ 
+
+$var = NULL;
+
+if ( is_int($var) )
+    print 'var is a integer';
+
+if ( is_float($var) )
+    print 'var is a float';
+
+if ( is_bool($var) )
+    print 'var is a boolean';
+
+if ( is_string($var) )
+    print 'var is a string';
+
+if ( is_null($var) )
+    print 'var is null';
+ 
