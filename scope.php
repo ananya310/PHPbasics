030914720bdfd771ba97379abf640c35530c66c3
Arindam Roy <arindamroy.roy619@live.com>
2014-08-28 03:49:52 +0530

    Create scope.php

---
+++ scope.php
@@ -0,0 +1,61 @@
+<?php
+
+/* 
+ * function & Variable scope.
+ */
+
+
+
+
+/*
+function construct_message($name) {
+    
+    global $message;
+    $message = 'Hi there from ' . $name . '!';
+    
+}
+
+
+function display_message() {
+    
+    //local var
+    //$message = 'Hi there!';
+    
+    global $message;
+    
+    print $message;
+    
+}
+
+construct_message("Biswajeet");
+display_message();
+
+function get_val($a,$b) {
+    global $val;
+    $val=$a*$b;
+}
+
+function printval() {
+    global $val;
+    print 'Value='.$val;
+    
+}
+get_val(8,6);
+printval();
+*/
+
+//function returning a value
+function get_my_value() {
+    
+    $value = "Hello from function";
+    
+    return $value;
+    
+    print '123456<br />';
+    
+}
+
+
+$new_value = get_my_value();
+
+print $new_value;
