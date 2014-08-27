16098660cb157f4cfb178480de283865c49cec3c
Arindam Roy <arindamroy.roy619@live.com>
2014-08-28 03:45:53 +0530

    Create conditional.php

---
+++ conditional.php
@@ -0,0 +1,74 @@
+<?php
+
+/* 
+ * Conditional
+ */
+
+
+/*
+$condition = 0;
+
+
+if ( $condition )
+    print 'Condition is true';
+
+else
+    print 'Condition is false';
+
+
+
+$condition2 = 12;
+
+
+if ( $condition2 == 1 )
+    print 'Condition is ' . $condition2;
+
+elseif ( $condition2 == 2 )
+        print 'Condition is ' . $condition2;
+
+else
+    print 'Condition is ' . $condition2;
+
+
+
+$my_value = "lsadfj";
+
+switch ($my_value) {
+    
+    case 'one':
+       print 'The value is: ' . $my_value;
+       break;
+   
+   case 'two':
+       print 'The value is: ' . $my_value;
+       break;
+   
+   case 'three':
+       print 'The value is: ' . $my_value;
+       break;
+      
+    default:
+        print 'No matched case';
+        break;
+    
+}
+*/
+
+//ternary operator
+/*
+$morning_time = 1;
+
+//$message = $morning_time ? 'Good morning!' : 'Good evening';
+print $morning_time ? 'Good morning!' : 'Good evening';
+
+
+
+$A = true;
+$B = true;
+
+if ( $A != $B )
+    print 'Condition is true';
+else
+    print 'Condition is false';
+
+*/
